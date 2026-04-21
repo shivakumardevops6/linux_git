67f4b43fbfee93ed55e3ab6bcfeb2bc504994e6a
shivakumarakm46 <shivakumarakm46@gmail.com>
2026-04-21 22:17:45 +0530

    +dir remove cmd

--- logical_operator.sh
+++ logical_operator.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
 mkdir test ; cd test ; echo "semicolun or Separator runs always even if command success or fail"
+
 pwd && ls -lrtha && echo "Logical AND Only if previous command success (status code exit (0) ) it continues to next command"
+
 kill -9 my-app-test || echo "App not running, best for error handling, it runs only if previous command fails"
 
+cd .. && rm -rf test
